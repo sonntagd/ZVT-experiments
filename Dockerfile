4254453623adbd7f0d467f5b53d7ab7ae6d8e241
Dominic Sonntag <dominic.sonntag@sonntag.it>
2017-06-22 19:59:48 +0200

    improved basic auth request

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,7 @@ FROM perl:5.20
 
 RUN cpanm IO::Socket::INET
 RUN cpanm DDP
-
+RUN apt-get update && apt-get install -y libpcap-dev
+RUN cpanm -f Net::Pcap
+RUN cpanm Net::Analysis
+RUN cpanm AnyEvent
--- req_auth.pl
+++ req_auth.pl
@@ -21,41 +21,41 @@ print "connected to the server\n";
 
 
 # REGISTRATION
-sendtoterminal($socket, '06 00', '00 00 00 08 09 78 03 00 06 06 26 04 0A 02 06 D3');
+sendtoterminal($socket, [qw{06 00}], [qw{00 00 00 08 09 78 03 00 06 06 26 04 0A 02 06 D3}]);
 my $response = receive($socket);
-die "got no okay :-(" if $response->{type} ne '80 00';
+die "got no okay :-(" if $response->{type}->[0] ne '80' or $response->{type}->[1] ne '00' or @{ $response->{type} } != 2;
 
 # awaiting reg answer, should be analyzed ...
 $response = receive($socket);
 
 # send okay
-sendtoterminal($socket, '80 00');
+sendtoterminal($socket, [qw{80 00}]);
 
 # request AUTHORIZATION
 my $amount = $ARGV[0] || 0.02;
-my $amount_hex = uc sprintf("%012d", $amount * 100) =~ s/(.{2})/$1 /gr;
-say $amount_hex;
-sendtoterminal($socket, '06 01', '04 '.$amount_hex.'19 40 06 04 40 02 FF 00');
+die "amount $amount is greater than allowed value (9999999999.99)" if $amount > 9999999999.99;
+my @amount_hex = ( uc(sprintf("%012d", int($amount * 100))) =~ m/../g );
+sendtoterminal($socket, [qw{06 01}], ['04', @amount_hex, qw{19 40 06 04 40 02 FF 00}]);
 $response = receive($socket);
-die "got no okay :-(" if $response->{type} ne '80 00';
+die "got no okay :-(" if $response->{type}->[0] ne '80' or $response->{type}->[1] ne '00' or @{ $response->{type} } != 2;
 
 
 $socket->close();
 
 
-sub hexify { join" " => map { sprintf('%02x', ord $_) } split //, $_[0] }
+sub hexify { [map { sprintf('%02x', ord $_) } split //, $_[0]] }
 
 # 06 01 0A 04 00 00 00 01 10 10 00 49 09 78 10 03 F2 FF
-sub unhexify { join "" => map { chr hex } split / /, $_[0] }
+sub unhexify { join "" => map { chr hex } @{$_[0]} }
 
 
 
 sub sendtoterminal {
     my ($socket, $type, $data) = @_;
-    $data ||= '';
+    $data ||= [];
     my $request = unhexify($type).chr(length unhexify($data)).unhexify($data);
     my $size = $socket->send($request);
-    say "sent data of length $size: ".hexify($request);
+    say "sent data of length $size: ".join(" " => @{ hexify($request) });
 }
 
 sub receive {
@@ -67,7 +67,7 @@ sub receive {
     my $data = "";
     $socket->recv( $data, $length ) if $length;
 
-    say "received data: ".hexify($response).' '.hexify($data);
+    say "received data: ".join(" " => @{ hexify($response) }, @{ hexify($data) });
 
     return {
         type   => hexify($type),
